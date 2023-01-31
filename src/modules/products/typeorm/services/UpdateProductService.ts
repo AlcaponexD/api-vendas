26ec529c4f2a7070ea4f61e7783a00fdf01870d6
Jeison Pedroso <jeison.contas@gmail.com>
2023-01-31 08:39:48 -0300

    create update  service using product repository

---
+++ src/modules/products/typeorm/services/UpdateProductService.ts
@@ -0,0 +1,42 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Product from '../entities/Product';
+import { ProductRepository } from '../repositories/ProductsRepository';
+
+interface IRequest {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+class UpdateProductService {
+  public async execute({
+    id,
+    name,
+    price,
+    quantity,
+  }: IRequest): Promise<Product> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const product = await productsRepository.findOne(id);
+    if (!product) {
+      throw new AppError('Product not found', 404);
+    }
+
+    const productExists = await productsRepository.findByName(name);
+    if (productExists && name !== product.name) {
+      throw new AppError('Products already exists with this name', 400);
+    }
+
+    product.name = name;
+    product.price = price;
+    product.quantity = quantity;
+
+    await productsRepository.save(product);
+
+    return product;
+  }
+}
+
+export default UpdateProductService;
